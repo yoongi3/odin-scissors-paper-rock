347167e90ca0a07c94445fc234845acbdf9c3f78
yoongi <lolhongy3@gmail.com>
2023-03-22 18:47:04 +1100

    create playRound function

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+
+const choices = ["scissors", "paper", "rock"];
+
 function getComputerChoice(){
-    const choices = ["scissors", "paper", "rock"]
 
     var randomComputerChoice = Math.floor(Math.random() * 3)
 
     return(choices[randomComputerChoice]);
 }
+function playRound (playerSelection, computerSelection){
 
-console.log(getComputerChoice());
+    const playerIndex = choices.indexOf(playerSelection);
+    const computerIndex = choices.indexOf(computerSelection);
+
+    if (playerIndex == computerIndex)
+        return "It's a draw!";
+    if (playerIndex - computerIndex == -1 || playerIndex - computerIndex == 2)
+        return "you win!";
+    else 
+        return "you lose!";
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+
+console.log("computer: " + computerSelection + " vs " + "player: " + playerSelection);
+console.log(playRound(playerSelection, computerSelection));
