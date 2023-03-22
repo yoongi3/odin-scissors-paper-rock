06ab4e5fa6bd5c453336f002a626baee97d7e592
yoongi <lolhongy3@gmail.com>
2023-03-22 19:51:38 +1100

    create game function and optimise playRound function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const choices = ["scissors", "paper", "rock"];
+//const results = ["lose", "draw", "win"];
 
 function getComputerChoice(){
 
@@ -7,21 +8,62 @@ function getComputerChoice(){
 
     return(choices[randomComputerChoice]);
 }
+
 function playRound (playerSelection, computerSelection){
 
     const playerIndex = choices.indexOf(playerSelection);
     const computerIndex = choices.indexOf(computerSelection);
 
-    if (playerIndex == computerIndex)
-        return "It's a draw!";
-    if (playerIndex - computerIndex == -1 || playerIndex - computerIndex == 2)
-        return "you win!";
-    else 
-        return "you lose!";
+    if (playerIndex == computerIndex){
+        return "draw";
+    }
+    if (playerIndex - computerIndex == -1 || playerIndex - computerIndex == 2){
+        return "win";
+    }
+    else{
+        return "lose";
+    }
 }
 
-const playerSelection = "scissors";
-const computerSelection = getComputerChoice();
+function game(){
+    const numberOfRounds = 5;
 
-console.log("computer: " + computerSelection + " vs " + "player: " + playerSelection);
-console.log(playRound(playerSelection, computerSelection));
+    var playerScore = 0;
+    var computerScore = 0;
+
+    for (i = 1; i <= numberOfRounds; i++){
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt();
+        const round = playRound(playerSelection, computerSelection);
+
+        console.log("Round " + i + ": player choice: " + playerSelection + " || computer choice: " + computerSelection)
+        
+        round;
+
+        if(round == "draw"){
+            console.log("This round was a draw.");
+            console.log("The Score is " + playerScore + " : " + computerScore);  
+        }
+        if(round == "win"){
+            playerScore += 1;
+            console.log("You win this round");
+            console.log("The Score is " + playerScore + " : " + computerScore);  
+
+        }
+        if(round == "lose"){
+            computerScore += 1;
+            console.log("You lose this round");
+            console.log("The Score is " + playerScore + " : " + computerScore);  
+        }
+    }
+
+    console.log("The final score is " + playerScore + " : " + computerScore);
+    if (playerScore == computerScore)
+        console.log("It's a Draw :)");
+    if (playerScore > computerScore)
+        console.log("You Win :D");
+    if (playerScore < computerScore)
+        console.log("You Lose :(");   
+}
+
+game();
