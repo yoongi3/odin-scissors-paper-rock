6e46cc745eae863ea2a5c2ea2221f45aefc9b1a0
yoongi <lolhongy3@gmail.com>
2023-04-08 00:35:11 +1000

    displat running score and annouce winner

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ const choices = ["scissors", "paper", "rock"];
 let rockBtn = document.getElementById('rock');
 let paperBtn = document.getElementById('paper');
 let scissorsBtn = document.getElementById('scissors');
+
+const container = document.querySelector('.result');
+const result = document.createElement('div');
+const scoreboard = document.createElement('div');
+container.appendChild(result);
+container.appendChild(scoreboard);
+
 let round = 0;
+let playerScore = 0;
+let computerScore = 0;
+
 rockBtn.addEventListener("click", function() {
     playerSelection = "rock";
     playRound(playerSelection);
@@ -27,15 +37,12 @@ function getComputerChoice(){
     computerSelection = choices[randomComputerChoice];
 }
 
-const container = document.querySelector('.result');
-const newDiv = document.createElement('div');    
-container.appendChild(newDiv);
 
 function playRound (playerSelection){
     round += 1;
 
-    while (newDiv.firstChild)
-        newDiv.firstChild.remove();
+    while (result.firstChild)
+        result.firstChild.remove();
 
     getComputerChoice();
 
@@ -46,17 +53,44 @@ function playRound (playerSelection){
 
     if (playerIndex == computerIndex){
         const newContent = document.createTextNode(currentRound + "It's a draw :|");
-        newDiv.appendChild(newContent);
-        return "draw";
+        result.appendChild(newContent);
+        updateScore();
+        return;
     }
     if (playerIndex - computerIndex == -1 || playerIndex - computerIndex == 2){
         const newContent = document.createTextNode(currentRound + "You win :)");
-        newDiv.appendChild(newContent);
-        return "win";
+        result.appendChild(newContent);
+        playerScore+=1;
+        updateScore();
+        return; 
     }
     else{
         const newContent = document.createTextNode(currentRound + "You lose :(");
-        newDiv.appendChild(newContent);
-        return "lose";
+        result.appendChild(newContent);
+        computerScore+=1;
+        updateScore();
+        return;
+    }
+}
+
+function updateScore(){
+    while (scoreboard.firstChild)
+        scoreboard.firstChild.remove();
+
+    let score = playerScore+" : "+computerScore;
+    let scoreContent = document.createTextNode("o");
+    if(playerScore >= 5){
+        scoreContent = document.createTextNode(score + " The player wins! :) ~Game Over~");
+        scoreboard.appendChild(scoreContent);
+        return;
+    }
+    if(computerScore >= 5){
+        scoreContent = document.createTextNode(score + " The computer wins! :( ~Game Over~");
+        scoreboard.appendChild(scoreContent);
+        return;
+    }
+    else{
+        scoreContent = document.createTextNode(score);
+        scoreboard.appendChild(scoreContent);
     }
 }
